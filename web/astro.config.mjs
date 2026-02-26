937ec3405066b5097ad5ee0a451caba4b13eae8d
vjrasane <3148501+vjrasane@users.noreply.github.com>
2026-02-26 18:08:05 +0200

    fix campaigns

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
+const isProd = process.env.NODE_ENV === "production";
+
 export default defineConfig({
+  site: isProd ? "https://vjrasane.github.io" : undefined,
+  base: isProd ? "/otherworld-codex" : undefined,
   integrations: [react()],
 });
