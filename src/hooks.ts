d0c3d14b4732dab4101bc9f6f7adf9acc81032e2
vjrasane <3148501+vjrasane@users.noreply.github.com>
2026-03-02 11:01:01 +0200

    fix cache

--- src/hooks.ts
+++ src/hooks.ts
@@ -5,13 +5,15 @@ import { SITE_TITLE } from "./config";
 const cache: Record<string, any> = {};
 
 export function useCachedData<T>(route: string, hash: string): T | null {
-  const cacheKey = [kebabCase(SITE_TITLE), route, hash].join(":");
+  const prefix = [kebabCase(SITE_TITLE), route].join(":");
+  const cacheKey = [prefix, hash].join(":");
 
   const [data, setData] = useState<T | null>((cache[cacheKey] as T) ?? null);
 
   const fetchData = async () => {
     const res = await fetch(route);
     const data = await res.json();
+    clearStaleCache(prefix);
     localStorage.setItem(cacheKey, JSON.stringify(data));
     cache[cacheKey] = data;
     setData(data);
@@ -21,7 +23,7 @@ export function useCachedData<T>(route: string, hash: string): T | null {
     let cached = cache[cacheKey];
     if (cached) return cached;
     cached = localStorage.getItem(cacheKey);
-    if (cached) JSON.parse(cached);
+    if (cached) return JSON.parse(cached);
     return null;
   };
 
@@ -32,4 +34,11 @@ export function useCachedData<T>(route: string, hash: string): T | null {
   }, []);
 
   return data;
+}
+
+function clearStaleCache(prefix: string) {
+  for (let i = 0; i < localStorage.length; i++) {
+    const key = localStorage.key(i);
+    if (key?.startsWith(prefix)) localStorage.removeItem(key);
+  }
 }
